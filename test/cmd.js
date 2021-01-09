0c8aab4cb64ad93c8b1e726e8eeaf6a4bd9b7773
gatecrasher777 <gatecrasher777@users.noreply.github.com>
2021-01-09 07:09:22 +0200

    Update cmd.js

--- test/cmd.js
+++ test/cmd.js
@@ -3,38 +3,39 @@
 var ht = require('../htflow.js')();
 var assert = require('assert');
 
-describe('Test cmd helper function', function() {
+describe('Test cmd helper function and ensure correct quote escaping', function() {
     
     var doSomething = function(a,b,c) {
         // event handler
     }
 
-    it('Should return an empty string if input is empty, undefined, or invalid', function() {
-        assert.strictEqual(ht.cmd(undefined),'');
-        assert.strictEqual(ht.cmd(undefined,undefined),'');
-        assert.strictEqual(ht.cmd(undefined,undefined,undefined),'');
-        assert.strictEqual(ht.cmd(null),'');
-        assert.strictEqual(ht.cmd(null,null),'');
-        assert.strictEqual(ht.cmd(null,null,null),'');
-        assert.strictEqual(ht.cmd([]),'');
-        assert.strictEqual(ht.cmd([],[]),'');
-        assert.strictEqual(ht.cmd([],[],[]),'');
-        assert.strictEqual(ht.cmd({}),'');
-        assert.strictEqual(ht.cmd({},{}),'');
-        assert.strictEqual(ht.cmd({},{},{}),'');
+    it('Should return an empty event string if input is empty, undefined, or invalid', function() {
+        assert.strictEqual(ht.img({onclick:ht.cmd()}),'<img onclick />');
+        assert.strictEqual(ht.img({onclick:ht.cmd(undefined)}),'<img onclick />');
+        assert.strictEqual(ht.img({onclick:ht.cmd(undefined,undefined)}),'<img onclick />');
+        assert.strictEqual(ht.img({onclick:ht.cmd(undefined,undefined,undefined)}),'<img onclick />');
+        assert.strictEqual(ht.img({onclick:ht.cmd(null)}),'<img onclick />');
+        assert.strictEqual(ht.img({onclick:ht.cmd(null,null)}),'<img onclick />');
+        assert.strictEqual(ht.img({onclick:ht.cmd(null,null,null)}),'<img onclick />');
+        assert.strictEqual(ht.img({onclick:ht.cmd([])}),'<img onclick />');
+        assert.strictEqual(ht.img({onclick:ht.cmd([],[])}),'<img onclick />');
+        assert.strictEqual(ht.img({onclick:ht.cmd([],[],[])}),'<img onclick />');
+        assert.strictEqual(ht.img({onclick:ht.cmd({})}),'<img onclick />');
+        assert.strictEqual(ht.img({onclick:ht.cmd({},{})}),'<img onclick />');
+        assert.strictEqual(ht.img({onclick:ht.cmd({},{},{})}),'<img onclick />');
     });
 
-    it('Should return a javascript command as an onevent reponse', function() {
-        assert.strictEqual(ht.cmd('doSomething'),'doSomething();');
-        assert.strictEqual(ht.cmd('doSomething','to a string'),'doSomething(\'to a string\');');
-        assert.strictEqual(ht.cmd('doSomething','to a string',369),'doSomething(\'to a string\',369);');
+    it('Should return a javascript command with optional parameters to an event handler', function() {
+        assert.strictEqual(ht.img({onclick:ht.cmd('doSomething')}),'<img onclick="doSomething();" />');
+        assert.strictEqual(ht.img({onclick:ht.cmd('doSomething','to a string')}),'<img onclick="doSomething(\"to a string\");" />');
+        assert.strictEqual(ht.img({onclick:ht.cmd('doSomething','to a string',369)}),'<img onclick="doSomething(\"to a string\",369);" />');
     });
 
     it('Should pass non-string/number parameters as declared/evaluated', function() {
-        assert.strictEqual(ht.cmd('doSomething',undefined,369),'doSomething(undefined,369);');
-        assert.strictEqual(ht.cmd('doSomething',undefined,NaN),'doSomething(undefined,NaN);');
-        assert.strictEqual(ht.cmd('doSomething',{an:'object'},['an','array']),'doSomething({"an":"object"},["an","array"]);');
-        assert.strictEqual(ht.cmd('doSomething',(3==6),(9==9),Infinity),'doSomething(false,true,Infinity);');
+        assert.strictEqual(ht.img({onclick:ht.cmd('doSomething',undefined,369)}),'<img onclick="doSomething(undefined,369);" />');
+        assert.strictEqual(ht.img({onclick:ht.cmd('doSomething',undefined,NaN)}),'<img onclick="doSomething(undefined,NaN);" />');
+        assert.strictEqual(ht.img({onclick:ht.cmd('doSomething',{an:'object'},['an','array'])}),'<img onclick="doSomething({\"an\":\"object\"},[\"an\",\"array\"]);" />');
+        assert.strictEqual(ht.img({onclick:ht.cmd('doSomething',(3==6),(9==9),Infinity)}),'<img onclick="doSomething(false,true,Infinity);" />');
     });
 
 });
