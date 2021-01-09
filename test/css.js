d58e33dfe869f709f4f1f53431314a6344579b17
gatecrasher777 <gatecrasher777@users.noreply.github.com>
2021-01-09 14:10:49 +0200

    Update css.js

--- test/css.js
+++ test/css.js
@@ -2,3 +2,29 @@
 
 var ht = require('../htflow.js')();
 var assert = require('assert');
+
+describe('Test css helper function', function(){
+
+    var myStyle = {
+        'max-height': '200px',
+        position: 'absolute'
+    }
+    
+    it('Should return an empty single tag if input is empty, undefined, invalid or a function', function() {
+        assert.strictEqual(ht.img({style:ht.css()}),'<img style />');
+        assert.strictEqual(ht.img({style:ht.css(undefined)}),'<img style />');
+        assert.strictEqual(ht.img({style:ht.css(null)}),'<img style />');
+        assert.strictEqual(ht.img({style:ht.css([])}),'<img style />');
+        assert.strictEqual(ht.img({style:ht.css({})}),'<img style />');
+        assert.strictEqual(ht.img({style:ht.css('howdy-doody')}),'<img style />');
+        assert.strictEqual(ht.img({style:ht.css(()=>{return myStyle})}),'<img style />');
+    });
+
+    it('Should interpret key/value pairs of an object parameter as style properties/values', function() {
+        assert.strictEqual(ht.img({style:ht.css({border:'1px solid red'})}),'<img style="border:1px solid red;" />')
+        assert.strictEqual(ht.img({style:ht.css(myStyle)}),'<img style="max-height:200px;position:absolute;" />');
+       
+        assert.strictEqual(ht.img({style:ht.css({a:1,b:'2',c:3,d:'4'})}),'<img style="a:1;b:2;c:3;d:4;" />');
+    });
+
+});
