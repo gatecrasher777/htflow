9df34a32f33d3fc0455e1dd5a1fc0f28f8c97066
gatecrasher777 <gatecrasher777@users.noreply.github.com>
2021-01-28 09:32:42 +0200

    Update htflow.js

--- htflow.js
+++ htflow.js
@@ -7,14 +7,14 @@
 class htflowClass {
 
 	constructor() {
-		var dtags = 'a,abbr,address,area,article,aside,audio,b,bdi,bdo,blockquote,body,button,'+
+		const dtags = 'a,abbr,address,area,article,aside,audio,b,bdi,bdo,blockquote,body,button,'+
 					'canvas,caption,cite,code,colgroup,data,datalist,dd,del,details,dfn,dialog,'+
 					'div,dl,dt,em,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,head,'+
 					'header,hgroup,html,i,iframe,ins,kbd,label,legend,li,main,map,mark,meter,nav,'+
 					'noscript,object,ol,optgroup,option,output,p,picture,pre,progress,q,rp,rt,rtc,'+
 					'ruby,s,samp,script,section,select,slot,small,span,strong,style,sub,summary,sup,'+
-					'svg,table,tbody,td,template,textarea,tfoot,th,thead,time,title,tr,u,ul,var,video';
-		var stags = 'base,br,col,embed,hr,img,input,link,meta,param,source,track,wbr';
+					'svg,table,tbody,td,template,textarea,tfoot,th,thead,time,title,tr,u,ul,const,video';
+		const stags = 'base,br,col,embed,hr,img,input,link,meta,param,source,track,wbr';
 		dtags.split(',').forEach( (t) => { 
 			this.doubleReg(t);
 		});
@@ -26,14 +26,14 @@ class htflowClass {
 	/* internal methods */
 
 	item(html,a,b,c) {
-		var h = '';
+		let h = '';
 		if (html === undefined) html = [];
 		if (!Array.isArray(html)) html = [html];
 		html.forEach((e)=>{
 			if (Array.isArray(e)) {
 				h+=this.item(e,a,b,c);
 			} else if (typeof e === "function") {
-				var s = e(a,b,c);
+				let s = e(a,b,c);
 				if (s === false) {
 					return false;
 				} else {
@@ -49,7 +49,7 @@ class htflowClass {
 	}
 	
 	attrStr(attr) {
-		var a = '';
+		let a = '';
 		if ((attr === undefined) || (attr === null)) return a;
 		Object.keys(attr).forEach( (k) => {
 			a += ' '+k;
@@ -59,8 +59,8 @@ class htflowClass {
 	}
 
 	doubleTag(tag,attr,html) {
-		var h = '<'+tag;
-		var a = this.attrStr(attr);
+		let h = '<'+tag;
+		const a = this.attrStr(attr);
 		(a.length) ? h += a+'>' : h += '>';
 		h += this.item(html,tag);
 		h += '</'+tag+'>';
@@ -68,8 +68,8 @@ class htflowClass {
 	} 
 	
 	singleTag(tag,attr) {
-		var h = '<'+tag;
-		var a = this.attrStr(attr);
+		let h = '<'+tag;
+		const a = this.attrStr(attr);
 		(a.length) ? h += a+' />' : h += '>';
 		return h;
 	}
@@ -78,20 +78,20 @@ class htflowClass {
 	
 	doubleReg(tag) {
 		this[tag] = function(a) {
-			var s=0;
-			var attr={};
+			let s=0;
+			let attr={};
 			if ((a !== undefined) && (typeof a === "object") && (!Array.isArray(a))) {
 				attr = a;
 				s = 1;
 			}
-			var html = Array.prototype.slice.call(arguments,s);
+			let html = Array.prototype.slice.call(arguments,s);
 			return this.doubleTag(tag,attr,html);
 		}
 	}
 	
 	singleReg(tag) {
 		this[tag] = function(a) {
-			var attr={};
+			let attr={};
 			if (typeof a === "object") {
 				attr = a;
 			}
@@ -100,7 +100,7 @@ class htflowClass {
 	}
 
 	doc(html) {
-		var h = '';
+		let h = '';
 		if (html!==undefined) h = this.item(html);
 		return '<DOCTYPE html>'+h;
 	}
@@ -115,9 +115,9 @@ class htflowClass {
 	
 	forLoop(start,end,html) { 
 		html = Array.prototype.slice.call(arguments,2);
-		var h = '';
-		var i = 0;
-		var s = '';
+		let h = '';
+		let i = 0;
+		let s = '';
 		if (start<end) {
 			for(i = start; i<=end; i++) {
 				s = this.item(html,i);
@@ -144,8 +144,8 @@ class htflowClass {
 		if (!Array.isArray(opts)) opts = [opts];
 		if (!Array.isArray(html)) html = [html];
 		if (opts.length == html.length) {
-			var usenext = false;
-			for (var i = 0; i < opts.length; i++) {
+			let usenext = false;
+			for (let i = 0; i < opts.length; i++) {
 				if( (val == opts[i]) || usenext) {
 					if (html[i] == '||') {
 						usenext = true;
@@ -161,7 +161,7 @@ class htflowClass {
 	
 	forEach(vals,html) {
 		if ((typeof vals ==='object') && (Array.isArray(vals))) {
-			var h='';
+			let h='';
 			html = Array.prototype.slice.call(arguments,1);
 			if (vals !== undefined) {
 				vals.forEach((e,i,a) => {
@@ -176,7 +176,7 @@ class htflowClass {
 	forIn(obj,html) {
 		if ((obj === undefined) || (obj === null)) return '';
 		if (typeof obj === 'object') {
-			var h = '';
+			let h = '';
 			html = Array.prototype.slice.call(arguments,1);
 			Object.keys(obj).forEach( (k) => {
 				h += this.item(html,k,obj[k]);
@@ -188,9 +188,9 @@ class htflowClass {
 	
 	whileDo(test,html) {
 		if (typeof test === 'function') {
-			var h = '';
+			let h = '';
 			html = Array.prototype.slice.call(arguments,1);
-			var cond = test();
+			let cond = test();
 			while(cond) {
 				h += this.item(html,cond);
 				cond = test();
@@ -202,9 +202,9 @@ class htflowClass {
 
 	doWhile(test,html) {
 		if (typeof test === 'function') {
-			var h = '';
+			let h = '';
 			html = Array.prototype.slice.call(arguments,1);
-			var cond;
+			let cond;
 			do {
 				h += this.item(html,cond);
 				cond = test();
@@ -215,7 +215,7 @@ class htflowClass {
 	}
 	
 	concat(html) {
-		var h = '';
+		let h = '';
 		html = Array.prototype.slice.call(arguments,0);
 		html.forEach((e)=>{
 			h += this.item(e);
@@ -225,10 +225,10 @@ class htflowClass {
 	
 	cmd(func,param) {
 		if (typeof func === 'string') {
-			var h = func+'(';
-			var v = '';
-			var j = ''
-			var args = Array.prototype.slice.call(arguments,1);
+			let h = func+'(';
+			let v = '';
+			let j = ''
+			const args = Array.prototype.slice.call(arguments,1);
 			args.forEach((e)=>{
 				if (e === undefined) {
 					v+=j+'undefined'
@@ -249,10 +249,10 @@ class htflowClass {
 
 	evt(func,param) {
 		if (typeof func === 'string') {
-			var h = func+'(';
-			var v = 'event';
-			var j = ','
-			var args = Array.prototype.slice.call(arguments,1);
+			let h = func+'(';
+			let v = 'event';
+			let j = ','
+			const args = Array.prototype.slice.call(arguments,1);
 			args.forEach((e)=>{
 				if (e === undefined) {
 					v+=j+'undefined'
@@ -273,7 +273,7 @@ class htflowClass {
 	css(prop) {
 		if ((prop === undefined) || (prop === null)) return '';
 		if ((typeof prop === 'object') && (!Array.isArray(prop))) {
-			var a = '';
+			let a = '';
 			Object.keys(prop).forEach( (k) => {
 				a += k+':'+prop[k]+';';
 			});
@@ -285,8 +285,8 @@ class htflowClass {
 }
 
 /* interface */
-// in client js,  var ht = htflowInit();
-// in nodejs, var ht = require('htflow')();
+// in client js,  const ht = htflowInit();
+// in nodejs, const ht = require('htflow')();
 
 function htflowInit() {
 	return new htflowClass;
