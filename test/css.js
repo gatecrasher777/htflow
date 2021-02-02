c69a879160b2292758036a8085a39735257dcdc5
gatecrasher777 <gatecrasher777@users.noreply.github.com>
2021-02-02 18:31:23 +0200

    Update css.js

--- test/css.js
+++ test/css.js
@@ -11,20 +11,20 @@ describe('Test css helper function', function(){
     }
     
     it('Should return an empty single tag if input is empty, undefined, invalid or a function', function() {
-        assert.strictEqual(ht.img({style:ht.css()}),'<img style />');
-        assert.strictEqual(ht.img({style:ht.css(undefined)}),'<img style />');
-        assert.strictEqual(ht.img({style:ht.css(null)}),'<img style />');
-        assert.strictEqual(ht.img({style:ht.css([])}),'<img style />');
-        assert.strictEqual(ht.img({style:ht.css({})}),'<img style />');
-        assert.strictEqual(ht.img({style:ht.css('howdy-doody')}),'<img style />');
-        assert.strictEqual(ht.img({style:ht.css(()=>{return myStyle})}),'<img style />');
+        assert.strictEqual(ht.img({style:ht.css()}),'<img style >');
+        assert.strictEqual(ht.img({style:ht.css(undefined)}),'<img style >');
+        assert.strictEqual(ht.img({style:ht.css(null)}),'<img style >');
+        assert.strictEqual(ht.img({style:ht.css([])}),'<img style >');
+        assert.strictEqual(ht.img({style:ht.css({})}),'<img style >');
+        assert.strictEqual(ht.img({style:ht.css('howdy-doody')}),'<img style >');
+        assert.strictEqual(ht.img({style:ht.css(()=>{return myStyle})}),'<img style >');
     });
 
     it('Should interpret key/value pairs of an object parameter as style properties/values', function() {
-        assert.strictEqual(ht.img({style:ht.css({border:'1px solid red'})}),'<img style="border:1px solid red;" />')
-        assert.strictEqual(ht.img({style:ht.css(myStyle)}),'<img style="max-height:200px;position:absolute;" />');
+        assert.strictEqual(ht.img({style:ht.css({border:'1px solid red'})}),'<img style="border:1px solid red;" >')
+        assert.strictEqual(ht.img({style:ht.css(myStyle)}),'<img style="max-height:200px;position:absolute;" >');
        
-        assert.strictEqual(ht.img({style:ht.css({a:1,b:'2',c:3,d:'4'})}),'<img style="a:1;b:2;c:3;d:4;" />');
+        assert.strictEqual(ht.img({style:ht.css({a:1,b:'2',c:3,d:'4'})}),'<img style="a:1;b:2;c:3;d:4;" >');
     });
 
 });
